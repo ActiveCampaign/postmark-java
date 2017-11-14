eafd31de556e469785a438a319e4b95872f81b21
Igor Balos <ibalosh@gmail.com>
2017-11-14 12:36:48 +0100

    added message endpoint tests

---
+++ src/test/java/MessagesTests.java
@@ -0,0 +1,62 @@
+import com.wildbit.java.postmark.client.ApiClient;
+import com.wildbit.java.postmark.client.Parameters;
+import com.wildbit.java.postmark.client.data.model.messages.*;
+import com.wildbit.java.postmark.client.data.model.server.Server;
+import com.wildbit.java.postmark.client.exception.PostmarkException;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+/**
+ * Created by bash on 11/14/17.
+ */
+public class MessagesTests extends BaseTest {
+
+    ApiClient client = getDefaultApiClient();
+
+    @Test
+    void list() throws PostmarkException, IOException {
+        OutboundMessages messages = client.getMessages(Parameters.init().build("count", 4).build("offset", 0));
+
+        assertNotNull(messages.getMessages().get(0).getMessageId());
+        assertTrue(messages.getMessages().size() > 0);
+    }
+
+    @Test
+    void messageById() throws PostmarkException, IOException {
+        OutboundMessages messages = client.getMessages(Parameters.init().build("count", 1).build("offset", 0));
+        String messageId = messages.getMessages().get(0).getMessageId();
+        OutboundMessageDetails message = client.getMessageDetails(messageId);
+
+        assertNotNull(message.getReceivedAt());
+    }
+
+    @Test
+    void messageByIdDump() throws PostmarkException, IOException {
+        OutboundMessages messages = client.getMessages(Parameters.init().build("count", 1).build("offset", 0));
+        String messageId = messages.getMessages().get(0).getMessageId();
+
+        OutboundMessageDump messageDump = client.getMessageDump(messageId);
+        assertNotNull(messageDump.getBody());
+    }
+
+    @Test
+    void listInbound() throws PostmarkException, IOException {
+        InboundMessages inboundMessages = client.getInboundMessages(Parameters.init().build("count", 6).build("offset", 0));
+
+        assertTrue(inboundMessages.getInboundMessages().size() > 0);
+    }
+
+    @Test
+    void inboundMessageById() throws PostmarkException, IOException {
+        InboundMessages inboundMessages = client.getInboundMessages(Parameters.init().build("count", 6).build("offset", 0));
+        String messageId = inboundMessages.getInboundMessages().get(0).getMessageId();
+
+        InboundMessageDetails inboundMessageDetails = client.getInboundMessageDetails(messageId);
+        assertNotNull(inboundMessageDetails.getFrom());
+    }
+}
