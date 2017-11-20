b5cd2678336feeced367ec1e0399a4f6ff6429ad
Igor Balos <ibalosh@gmail.com>
2017-11-20 11:05:02 +0100

    added base server checks

---
+++ src/test/java/ServerTest.java
@@ -0,0 +1,23 @@
+import com.wildbit.java.postmark.client.ApiClient;
+import com.wildbit.java.postmark.client.data.model.server.Server;
+import com.wildbit.java.postmark.client.exception.PostmarkException;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+/**
+ * Created by bash on 11/14/17.
+ */
+public class ServerTest extends BaseTest {
+
+    ApiClient client = getDefaultApiClient();
+
+    @Test
+    void list() throws PostmarkException, IOException {
+        Server server = client.getServer();
+        assertNotNull(server.getName());
+    }
+}
---
+++ src/test/java/ServersTest.java
@@ -0,0 +1,28 @@
+import com.wildbit.java.postmark.client.AccountApiClient;
+import com.wildbit.java.postmark.client.Parameters;
+import com.wildbit.java.postmark.client.data.model.server.Server;
+import com.wildbit.java.postmark.client.data.model.servers.Servers;
+import com.wildbit.java.postmark.client.exception.PostmarkException;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+/**
+ * Created by bash on 11/14/17.
+ */
+public class ServersTest extends BaseTest {
+
+    AccountApiClient client = getDefaultAccountApiClient();
+
+    @Test
+    void list() throws PostmarkException, IOException {
+        Servers servers = client.getServers(Parameters.init().build("count", 5).build("offset", 0));
+        assertTrue(servers.getServers().size() > 0);
+        assertTrue(servers.getTotalCount() > 0);
+    }
+
+}
