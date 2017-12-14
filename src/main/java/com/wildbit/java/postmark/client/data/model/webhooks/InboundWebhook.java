4264a3597de44828a14cc4004b0ad43607c09718
Igor Balos <ibalosh@gmail.com>
2017-12-14 14:14:48 +0100

    added separate inbound object for webhook for clearer naming

---
+++ src/main/java/com/wildbit/java/postmark/client/data/model/webhooks/InboundWebhook.java
@@ -0,0 +1,5 @@
+package com.wildbit.java.postmark.client.data.model.webhooks;
+
+import com.wildbit.java.postmark.client.data.model.messages.InboundMessageDetails;
+
+public class InboundWebhook extends InboundMessageDetails { }
