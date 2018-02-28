3a87010c922fd2f4eafe495ca12656c12c8251f6
Igor Balos <ibalosh@gmail.com>
2018-02-28 09:54:51 +0100

    updated data handler test

--- src/test/java/unit/client/data/DataHandlerTest.java
+++ src/test/java/unit/client/data/DataHandlerTest.java
@@ -43,6 +43,7 @@ public class DataHandlerTest {
 
     @Test
     void strictMapper() throws IOException {
+        dataHandler.setStrictMapper();
         Throwable exception = assertThrows(UnrecognizedPropertyException.class,
                 ()-> dataHandler.fromJson(getStringHashMap(), BaseMessageResponse.class));
 
