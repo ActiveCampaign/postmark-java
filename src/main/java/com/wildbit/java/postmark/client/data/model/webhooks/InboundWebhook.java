573d5c99a783a313e1b7f54729c1082f1bc80633
Igor Balos <ibalosh@gmail.com>
2018-04-03 10:51:12 +0200

    added raw email field support for inbound webhook

--- src/main/java/com/wildbit/java/postmark/client/data/model/webhooks/InboundWebhook.java
+++ src/main/java/com/wildbit/java/postmark/client/data/model/webhooks/InboundWebhook.java
@@ -2,4 +2,17 @@ package com.wildbit.java.postmark.client.data.model.webhooks;
 
 import com.wildbit.java.postmark.client.data.model.messages.InboundMessageDetails;
 
-public class InboundWebhook extends InboundMessageDetails { }
+public class InboundWebhook extends InboundMessageDetails {
+
+    private String rawEmail;
+
+    // GETTERS AND SETTERS
+
+    public String getRawEmail() {
+        return rawEmail;
+    }
+
+    public void setRawEmail(String rawEmail) {
+        this.rawEmail = rawEmail;
+    }
+}
