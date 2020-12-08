8e6f9e6905e79d1c5254368161e136eaadb648cf
Igor Balos <ibalosh@gmail.com>
2020-12-08 12:13:12 +0100

    remove obsolete test

--- src/test/java/integration/TemplatedMessageTest.java
+++ src/test/java/integration/TemplatedMessageTest.java
@@ -5,6 +5,7 @@ import com.wildbit.java.postmark.Postmark;
 import com.wildbit.java.postmark.client.ApiClient;
 import com.wildbit.java.postmark.client.data.model.message.Message;
 import com.wildbit.java.postmark.client.data.model.message.MessageResponse;
+import com.wildbit.java.postmark.client.data.model.templates.Template;
 import com.wildbit.java.postmark.client.data.model.templates.TemplatedMessage;
 import com.wildbit.java.postmark.client.exception.InvalidAPIKeyException;
 import com.wildbit.java.postmark.client.exception.InvalidMessageException;
@@ -20,14 +21,6 @@ import java.util.Map;
 public class TemplatedMessageTest extends BaseTest {
 
     ApiClient client = Postmark.getApiClient("POSTMARK_API_TEST", true);
-
-    @Test
-    void send() throws PostmarkException, IOException {
-        TemplatedMessage message = new TemplatedMessage("from@example.com", "to@example.com", 1);
-
-        Throwable exception = assertThrows(InvalidMessageException.class, () -> client.deliverMessageWithTemplate(message));
-        assertEquals("The 'TemplateId' associated with this request is not valid or was not found.", exception.getMessage());
-    }
 
     @Test
     void metadata() throws PostmarkException, IOException {
