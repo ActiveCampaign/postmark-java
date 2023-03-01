1d1a8814714b39e52abfb7531ad6a5672d2a794e
Igor Balos <ibalosh@gmail.com>
2023-03-01 15:53:11 +0100

    remove dependency on javax packages from jersey jax-rs client and fix PATCH request for newer versions of Java

--- src/main/java/com/postmarkapp/postmark/client/HttpClient.java
+++ src/main/java/com/postmarkapp/postmark/client/HttpClient.java
@@ -1,14 +1,15 @@
 package com.postmarkapp.postmark.client;
 
-import org.glassfish.jersey.client.ClientConfig;
-import org.glassfish.jersey.client.ClientProperties;
-import org.glassfish.jersey.client.HttpUrlConnectorProvider;
+import org.apache.hc.client5.http.config.RequestConfig;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
+import org.apache.hc.client5.http.impl.classic.HttpClientBuilder;
+import org.apache.hc.client5.http.impl.classic.HttpClients;
+import org.apache.hc.core5.http.ClassicHttpRequest;
+import org.apache.hc.core5.http.io.entity.EntityUtils;
+import org.apache.hc.core5.http.io.support.ClassicRequestBuilder;
+import org.apache.hc.core5.util.Timeout;
 
-import javax.ws.rs.client.Client;
-import javax.ws.rs.client.ClientBuilder;
-import javax.ws.rs.client.Entity;
-import javax.ws.rs.client.Invocation.Builder;
-import javax.ws.rs.core.Response;
+import java.io.IOException;
 import java.util.Map;
 
 /**
@@ -30,60 +31,22 @@ public class HttpClient {
     }
 
     private final Map<String,Object> headers;
-    private final Client client;
+    private CloseableHttpClient client;
 
     private boolean secureConnection = true;
+    private int connectTimeoutSeconds = DEFAULTS.CONNECT_TIMEOUT_SECONDS.value;
+    private int readTimeoutSeconds = DEFAULTS.READ_TIMEOUT_SECONDS.value;
 
     public HttpClient(Map<String,Object> headers, int connectTimeoutSeconds, int readTimeoutSeconds) {
-        this(headers);
-        setConnectTimeoutSeconds(connectTimeoutSeconds);
-        setReadTimeoutSeconds(readTimeoutSeconds);
+        this.headers = headers;
+        this.connectTimeoutSeconds = connectTimeoutSeconds;
+        this.readTimeoutSeconds = readTimeoutSeconds;
+        buildClientWithCustomConfig();
     }
 
     public HttpClient(Map<String,Object> headers) {
         this.headers = headers;
-        this.client = buildClient();
-        setReadTimeoutSeconds(DEFAULTS.READ_TIMEOUT_SECONDS.value);
-        setConnectTimeoutSeconds(DEFAULTS.CONNECT_TIMEOUT_SECONDS.value);
-    }
-
-    /**
-     *
-     * Execute HTTP request.
-     *
-     * @param requestType type of HTTP request to initiate
-     * @param url request url
-     * @param data data sent for POST/PUT requests
-     * @return response from HTTP request
-     */
-    public ClientResponse execute(REQUEST_TYPES requestType, String url, String data) {
-        Response response;
-        final Builder requestBuilder = clientRequestBuilder((url));
-
-        switch (requestType) {
-            case POST:
-                response = requestBuilder.post(Entity.json(data), Response.class);
-                break;
-
-            case PUT:
-                response = requestBuilder.put(Entity.json(data), Response.class);
-                break;
-
-            case PATCH:
-                response = requestBuilder.method("PATCH", Entity.json(data), Response.class);
-                break;
-
-            case DELETE:
-                response = requestBuilder.delete(Response.class);
-                break;
-
-            default:
-                response = requestBuilder.get(Response.class);
-                break;
-
-        }
-
-        return transformResponse(response);
+        buildClientWithCustomConfig();
     }
 
     /**
@@ -95,18 +58,63 @@ public class HttpClient {
      *
      * @see #execute(REQUEST_TYPES, String, String) for details
      */
-    public ClientResponse execute(REQUEST_TYPES request_type, String url) {
+    public ClientResponse execute(REQUEST_TYPES request_type, String url) throws IOException {
         return execute(request_type, url, null);
+    }
+
+    /**
+     *
+     * Execute HTTP request.
+     *
+     * @param requestType type of HTTP request to initiate
+     * @param url request url
+     * @param data data sent for POST/PUT requests
+     * @return response from HTTP request
+     */
+    public ClientResponse execute(REQUEST_TYPES requestType, String url, String data) throws IOException {
+        ClassicHttpRequest request;
+
+        switch (requestType) {
+            case POST:
+                request = ClassicRequestBuilder.post(getHttpUrl(url)).setEntity(data).build();
+                break;
+
+            case PUT:
+                request = ClassicRequestBuilder.put(getHttpUrl(url)).setEntity(data).build();
+                break;
+
+            case PATCH:
+                request = ClassicRequestBuilder.patch(getHttpUrl(url)).setEntity(data).build();
+                break;
+
+            case DELETE:
+                request = ClassicRequestBuilder.delete(getHttpUrl(url)).build();
+                break;
+
+            default:
+                request = ClassicRequestBuilder.get(getHttpUrl(url)).build();
+                break;
+        }
+
+        for (Map.Entry<String, Object> header : headers.entrySet()) {
+            request.setHeader(header.getKey(), header.getValue().toString());
+        }
+
+        return client.execute(
+                request,
+                response -> new ClientResponse(response.getCode(), EntityUtils.toString(response.getEntity())));
     }
 
     // Setters and Getters
 
     public void setConnectTimeoutSeconds(int connectTimeoutSeconds) {
-        client.property(ClientProperties.CONNECT_TIMEOUT, connectTimeoutSeconds * 1000);
+        this.connectTimeoutSeconds = connectTimeoutSeconds;
+        buildClientWithCustomConfig();
     }
 
     public void setReadTimeoutSeconds(int readTimeoutSeconds) {
-        client.property(ClientProperties.READ_TIMEOUT, readTimeoutSeconds * 1000);
+        this.readTimeoutSeconds = readTimeoutSeconds;
+        buildClientWithCustomConfig();
     }
 
     public void setSecureConnection(boolean secureConnection) {
@@ -123,59 +131,32 @@ public class HttpClient {
      *
      * @return original HTTP client
      */
-    public Client getClient() {
+    public CloseableHttpClient getClient() {
         return client;
     }
 
     /**
      * Build default HTTP client used for requests
      *
-     * Jersey client uses HttpUrlConnection by default which doesn't have PATCH method:
-     * https://docs.oracle.com/javase/8/docs/api/java/net/HttpURLConnection.html#setRequestMethod-java.lang.String-
-     * https://github.com/eclipse-ee4j/jersey/issues/4825
-     *
-     * Workaround for being able to do PATCH requests is by using reflection, which would work up to Java 16.
-     * https://stackoverflow.com/questions/55778145/how-to-use-patch-method-with-jersey-invocation-builder
+     * @return initialized HTTP client
+     */
+    private CloseableHttpClient buildClient() {
+        return HttpClients.createDefault();
+    }
+
+    /**
+     * Build HTTP client used for requests with custom config settings
      *
      * @return initialized HTTP client
      */
-    private Client buildClient() {
-        Client client = ClientBuilder.newClient();
-        // this allows calls to PATCH by using reflection
-        client.property(HttpUrlConnectorProvider.SET_METHOD_WORKAROUND, true);
-        return client;
-    }
+    private void buildClientWithCustomConfig() {
+        RequestConfig requestConfig = RequestConfig
+                .custom()
+                .setConnectTimeout(Timeout.ofSeconds(connectTimeoutSeconds))
+                .setResponseTimeout(Timeout.ofSeconds(readTimeoutSeconds))
+                .build();
 
-    private Builder clientRequestBuilder(String url) {
-        Builder requestBuilder = client.target(getHttpUrl(url)).request();
-
-        for (Map.Entry<String, Object> header : headers.entrySet()) {
-            requestBuilder.header(header.getKey(), header.getValue());
-        }
-
-        return requestBuilder;
-    }
-
-    /**
-     * Build HTTP client with custom config used for requests
-     * like:
-     *
-     * ClientConfig clientConfig = new ClientConfig();
-     * clientConfig.connectorProvider(new GrizzlyConnectorProvider());
-     * clientConfig.connectorProvider(new HttpUrlConnectorProvider().useSetMethodWorkaround());
-     */
-    private Client buildClient(ClientConfig config) {
-        return ClientBuilder.newClient(config);
-    }
-
-    /**
-     *  Gets simplified HTTP request response that will contain only response and status.
-     *
-     * @param response HTTP request response result
-     * @return simplified HTTP request response
-     */
-    private ClientResponse transformResponse(Response response) {
-        return new ClientResponse(response.getStatus(), response.readEntity(String.class));
+        this.client = HttpClientBuilder.create().setDefaultRequestConfig(requestConfig).build();
     }
 
     /**
